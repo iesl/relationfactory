c393897f33002ce51eb9d116d966b1afe8b34d63
davidBelanger <david.b.belanger@gmail.com>
2015-04-23 12:31:16 -0400

    revisiting EP stuff

---
+++ collectEP.pl
@@ -0,0 +1,56 @@
+use strict;
+
+open(I,$ARGV[0])||die;
+open(L,$ARGV[1])||die;
+open(OS,">$ARGV[2]")||die;
+open(OL,">$ARGV[3]")||die;
+
+my $prototype_lhs = "";
+my @cur_labels = ();
+my @cur_sentences = ();
+my $cur_EP = "";
+
+my $cnt = 0;
+while(<I>){
+    chomp;
+    my $lhs = <L>;
+    my @fields = split("\t");
+    my $ep = $fields[0];
+    my $label = $fields[1];
+    my $sent = $fields[2];
+
+    if($ep eq $cur_EP){
+	push(@cur_sentences, $sent);
+	push(@cur_labels,$label);
+    }else{
+	if($cnt > 0){
+	    my @cur_labels_mod = uniq @cur_labels; #deduplicate
+	    print OS join(",",@cur_labels_mod)."\t".join(",",@cur_sentences)."\n";
+	    print OL $prototype_lhs;
+	}
+	#reset
+	@cur_labels = ();
+	@cur_sentences = ();
+	$cur_EP = $ep;
+	$prototype_lhs = $lhs;
+
+	push(@cur_sentences, $sent);
+	push(@cur_labels,$label);
+    }
+    $cnt++;
+}
+close(I);
+close(L);
+
+foreach $name (@names){
+    die unless(exists $prototypes{$name});
+    if(exists $examples{$name}){
+	my $prototype = $prototypes{$name};
+	my @curFeats = @{$examples{$name}};
+	print $prototype."\t".join("&&&",@curFeats)."&&&\n";
+    }else{
+	die "failed: $name\n";
+    }
+
+}
+

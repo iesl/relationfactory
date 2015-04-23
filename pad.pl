c3292e4a80154850dda2739c1eba0bc01d12a6f0
davidBelanger <david.b.belanger@gmail.com>
2015-04-23 12:59:10 -0400

    adding pad

---
+++ pad.pl
@@ -0,0 +1,21 @@
+open(I,$ARGV[0])||die;
+my $psize = $ARGV[1] + 0;
+
+my $p = "<PAD>";
+
+while(<I>){
+    chomp;
+    my @fields = split("\t");
+    my @sent = split(" ",$fields[2]);
+    my $bit = 1;
+    while(scalar(@sent) < $psize){
+	if($bit){
+	    push(@sent,$p);
+	}else{
+	    unshift(@sent,$p);
+	}
+	$bit = 1- $bit;
+    }
+    my $ss = join(" ",@sent);
+    print $fields[0]."\t".$fields[1].$ss."\n";
+}

12e72e2252ab1e4a2a56a6570e3dd31cacca2c3d
Benjamin Roth <beroth@cs.umass.edu>
2015-06-16 15:14:31 -0400

    nn tuning

---
+++ components/bin/experimental/tune_nn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export rundir=$TAC_ROOT/runs/tune_nn
+params="0.5 0.6 0.7 0.8 0.9 0.95 1.0"
+
+cp /iesl/canvas/beroth/tac/data/candidates2009-2012 $rundir/candidates
+
+for param in $params
+do
+ export threshold=$param
+ $TAC_ROOT/bin/run.sh $TAC_ROOT/config/tune_nn.config
+ mv $rundir/response_nn_pp12 $rundir/response_nn_pp12.$param
+ rm $rundir/prediction*
+done
+
+key=/iesl/canvas/beroth/tac/data/key_2009-2012
+
+/iesl/canvas/belanger/relationfactory/myEvaluation/bin/tunej.sh $key $rundir/response_nn_pp12. $params
+

a258ac48d5b7b2110a683c26ba18af220faadbde
Benjamin Roth <beroth@compute-1-12.blake>
2015-06-17 21:31:23 -0400

    nn tuning

--- bin/tune_nn.mk
+++ bin/tune_nn.mk
@@ -5,18 +5,6 @@
 #
 # Author: Benjamin Roth
 
-# copies query.xml from location specified in config file
-#query.xml:
-	cp $(shell $(TAC_ROOT)/bin/get_expand_config.sh query.xml) $@
-
-# Adds expansiond to original queries and explicitly lists relations.
-#query_expanded.xml: query.xml
-	$(TAC_ROOT)/components/bin/expand_query.sh $+ $@
-
-# Candidates from sentences where Query string and tags match.
-candidates: query_expanded.xml dtag dscore
-	$(TAC_ROOT)/components/bin/candidates2013.sh $+ $@
-
 # Neural network predictions & response.
 predictions_nn: candidates
 	$(TAC_ROOT)/components/bin/predictions_nn.sh $+ $@
@@ -24,18 +12,8 @@ predictions_nn: candidates
 response_nn: query_expanded.xml predictions_nn
 	$(TAC_ROOT)/components/bin/response.sh $+ $@
 
-# Postprocess response for 2014 format.
-%_pp14: % query_expanded.xml /dev/null
-	$(TAC_ROOT)/components/bin/postprocess2014.sh $+ $@
-
-# Template for postprocessing responses for 2013 format
-#%_pp13: % query_expanded.xml title_org.tabs
-%_pp13: % query_expanded.xml /dev/null
-	$(TAC_ROOT)/components/bin/postprocess2013.sh $+ $@
-
-
 # Postprocess response for 2012 format
 %_pp12: % query_expanded.xml
-	$(TAC_ROOT)/components/bin/postprocess2012.sh $+ $@
+	$(TAC_ROOT)/components/bin/postprocess.sh $+ $@
 
 
